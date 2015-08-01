627a8d44e68af394feb1075ae03e24aca6af1254
lstypka <lukasz.stypka@gmail.com>
2015-08-01 20:01:12 +0200

    Added tests

---
+++ src/main/java/pl/jsolve/templ4docx/core/RunExtractor.java
@@ -0,0 +1,82 @@
+package pl.jsolve.templ4docx.core;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+import pl.jsolve.sweetener.text.Strings;
+
+public class RunExtractor {
+
+    public void execute(List<XWPFRun> runs, String[] keyArray, String[] valueArray, VariablePattern variablePattern) {
+        if (runs == null || runs.isEmpty()) {
+            return;
+        } else if (runs.size() == 1) {
+            XWPFRun xwpfRun = runs.get(0);
+            String text = xwpfRun.getText(0);
+            if (text != null) {
+                text = StringUtils.replaceEach(text, keyArray, valueArray);
+                xwpfRun.setText(text, 0);
+            }
+        } else {
+            for (XWPFRun xwpfRun : runs) {
+                String text = xwpfRun.getText(0);
+                if (text != null) {
+                    text = StringUtils.replaceEach(text, keyArray, valueArray);
+                    xwpfRun.setText(text, 0);
+                }
+            }
+
+            // validate whether xwpfRun contains any variable pattern which are not recognized
+            String notRecognizedVariable = "";
+            String notRecognizedVariablePrefix = "";
+            int notRecognizedVariableStartIndex = -1;
+            for (int i = 0; i < runs.size(); i++) {
+                String text = runs.get(i).getText(0);
+                if (text != null) {
+                    text = text.trim();
+                    // check whether variable is started but not ended
+
+                    if (notRecognizedVariableStartIndex != -1) {
+                        if (!Strings.indexesOf(text, variablePattern.getSuffix()).isEmpty()) {
+                            notRecognizedVariable += text.substring(0,
+                                    Strings.indexesOf(text, variablePattern.getSuffix()).get(0) + 1);
+                            notRecognizedVariable = StringUtils
+                                    .replaceEach(notRecognizedVariable, keyArray, valueArray);
+                            XWPFRun startRun = runs.get(notRecognizedVariableStartIndex);
+                            String textFromStartRun = startRun.getText(0);
+                            textFromStartRun = textFromStartRun.replace(notRecognizedVariablePrefix,
+                                    notRecognizedVariable);
+                            startRun.setText(textFromStartRun, 0);
+
+                            // clean runs between start and end variable pattern
+                            for (int j = notRecognizedVariableStartIndex + 1; j < i; j++) {
+                                runs.get(j).setText("", 0);
+                            }
+                            text = runs.get(i).getText(0);
+                            Integer suffixIndex = Strings.indexesOf(text, variablePattern.getSuffix()).get(0);
+                            runs.get(i).setText(text.substring(suffixIndex + 1), 0);
+                            i = notRecognizedVariableStartIndex;
+
+                            notRecognizedVariableStartIndex = -1;
+                            notRecognizedVariable = "";
+                            notRecognizedVariablePrefix = "";
+                            continue;
+                        }
+                        notRecognizedVariable += text;
+                    }
+
+                    if (!Strings.indexesOf(text, variablePattern.getPrefix()).isEmpty()
+                            && Strings.indexesOf(text, variablePattern.getSuffix()).isEmpty()) {
+                        notRecognizedVariableStartIndex = i;
+                        notRecognizedVariable = text;
+                        Integer prefixIndex = Strings.indexesOf(text, variablePattern.getPrefix()).get(0);
+                        notRecognizedVariablePrefix = text.substring(prefixIndex);
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ src/test/java/pl/jsolve/templ4doc/core/RunExtractorTest.java
@@ -0,0 +1,129 @@
+package pl.jsolve.templ4doc.core;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.List;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.junit.Before;
+import org.junit.Test;
+
+import pl.jsolve.sweetener.collection.Collections;
+import pl.jsolve.templ4docx.core.RunExtractor;
+import pl.jsolve.templ4docx.core.VariablePattern;
+
+public class RunExtractorTest {
+
+    private RunExtractor extractor;
+    private XWPFDocument document;
+    private XWPFParagraph paragraph;
+
+    @Before
+    public void setUp() {
+        document = new XWPFDocument();
+        paragraph = document.createParagraph();
+        extractor = new RunExtractor();
+    }
+
+    @Test
+    public void shouldReplaceVariable1() throws Exception {
+        // given
+        List<XWPFRun> runs = prepareRuns("${name}");
+        String[] keysArray = {"${age}", "${name}"};
+        String[] valuesArray = {"15", "Lukasz"};
+        VariablePattern variablePattern = new VariablePattern("${", "}");
+
+        // when
+        extractor.execute(runs, keysArray, valuesArray, variablePattern);
+
+        // then
+        assertThat(runs).hasSize(1);
+        assertThat(runs.get(0).getText(0)).isEqualTo("Lukasz");
+    }
+
+    @Test
+    public void shouldReplaceVariable2() throws Exception {
+        // given
+        List<XWPFRun> runs = prepareRuns("${", "name}");
+        String[] keysArray = {"${age}", "${name}"};
+        String[] valuesArray = {"15", "Lukasz"};
+        VariablePattern variablePattern = new VariablePattern("${", "}");
+
+        // when
+        extractor.execute(runs, keysArray, valuesArray, variablePattern);
+
+        // then
+        assertThat(runs).hasSize(2);
+        assertThat(runs.get(0).getText(0)).isEqualTo("Lukasz");
+        assertThat(runs.get(1).getText(0)).isEqualTo("");
+    }
+
+    @Test
+    public void shouldReplaceVariable3() throws Exception {
+        // given
+        List<XWPFRun> runs = prepareRuns("${", "name", "}");
+        String[] keysArray = {"${age}", "${name}"};
+        String[] valuesArray = {"15", "Lukasz"};
+        VariablePattern variablePattern = new VariablePattern("${", "}");
+
+        // when
+        extractor.execute(runs, keysArray, valuesArray, variablePattern);
+
+        // then
+        assertThat(runs).hasSize(3);
+        assertThat(runs.get(0).getText(0)).isEqualTo("Lukasz");
+        assertThat(runs.get(1).getText(0)).isEqualTo("");
+        assertThat(runs.get(2).getText(0)).isEqualTo("");
+    }
+
+    @Test
+    public void shouldReplaceVariable4() throws Exception {
+        // given
+        List<XWPFRun> runs = prepareRuns("${", "name", "}${", "age}");
+        String[] keysArray = {"${age}", "${name}"};
+        String[] valuesArray = {"15", "Lukasz"};
+        VariablePattern variablePattern = new VariablePattern("${", "}");
+
+        // when
+        extractor.execute(runs, keysArray, valuesArray, variablePattern);
+
+        // then
+        assertThat(runs).hasSize(4);
+        assertThat(runs.get(0).getText(0)).isEqualTo("Lukasz");
+        assertThat(runs.get(1).getText(0)).isEqualTo("");
+        assertThat(runs.get(2).getText(0)).isEqualTo("15");
+        assertThat(runs.get(3).getText(0)).isEqualTo("");
+    }
+
+    @Test
+    public void shouldReplaceVariable5() throws Exception {
+        // given
+        List<XWPFRun> runs = prepareRuns("${", "name", "}${", "age", "}");
+        String[] keysArray = {"${age}", "${name}"};
+        String[] valuesArray = {"15", "Lukasz"};
+        VariablePattern variablePattern = new VariablePattern("${", "}");
+
+        // when
+        extractor.execute(runs, keysArray, valuesArray, variablePattern);
+
+        // then
+        assertThat(runs).hasSize(5);
+        assertThat(runs.get(0).getText(0)).isEqualTo("Lukasz");
+        assertThat(runs.get(1).getText(0)).isEqualTo("");
+        assertThat(runs.get(2).getText(0)).isEqualTo("15");
+        assertThat(runs.get(3).getText(0)).isEqualTo("");
+        assertThat(runs.get(4).getText(0)).isEqualTo("");
+    }
+
+    private List<XWPFRun> prepareRuns(String... values) {
+        List<XWPFRun> runs = Collections.newArrayList();
+        for (String value : values) {
+            XWPFRun run = paragraph.createRun();
+            run.setText(value);
+            runs.add(run);
+        }
+        return runs;
+    }
+}
