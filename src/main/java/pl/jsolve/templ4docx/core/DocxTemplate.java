33ca1d60342d1ca8ded0b17d4a99a18af076ab24
lstypka <lukasz.stypka@gmail.com>
2015-08-01 13:42:45 +0200

    Fixed problem with multiple runs

--- src/main/java/pl/jsolve/templ4docx/core/DocxTemplate.java
+++ src/main/java/pl/jsolve/templ4docx/core/DocxTemplate.java
@@ -11,72 +11,68 @@ import pl.jsolve.sweetener.io.Resources;
 import pl.jsolve.templ4docx.exception.OpenDocxException;
 
 /**
- * 
- * The main class responsible for reading docx template, finding variables,
- * saving completed docx template
- * 
- * @author £ukasz Stypka
- * 
+ * The main class responsible for reading docx template, finding variables, saving completed docx template
+ * @author ï¿½ukasz Stypka
  */
 public class DocxTemplate {
 
-	private VariablePattern variablePattern = new VariablePattern("${", "}");
+    private VariablePattern variablePattern = new VariablePattern("${", "}");
 
-	public Docx openTemplate(String path) {
-		return new Docx(path);
-	}
+    public Docx openTemplate(String path) {
+        return new Docx(path);
+    }
 
-	public String readTextContent(Docx docx) {
-		XWPFWordExtractor extractor = null;
-		try {
-			extractor = new XWPFWordExtractor(docx.getDocument());
-			return extractor.getText();
-		} finally {
-			if (extractor != null) {
-				Resources.closeStream(extractor);
-			}
-		}
-	}
+    public String readTextContent(Docx docx) {
+        XWPFWordExtractor extractor = null;
+        try {
+            extractor = new XWPFWordExtractor(docx.getDocument());
+            return extractor.getText();
+        } finally {
+            if (extractor != null) {
+                Resources.closeStream(extractor);
+            }
+        }
+    }
 
-	public List<String> findVariables(Docx docx) {
-		VariablesExtractor extractor = new VariablesExtractor();
-		String content = readTextContent(docx);
-		return extractor.extract(content, variablePattern);
-	}
+    public List<String> findVariables(Docx docx) {
+        VariablesExtractor extractor = new VariablesExtractor();
+        String content = readTextContent(docx);
+        return extractor.extract(content, variablePattern);
+    }
 
-	public Docx fillTemplate(String path, Map<String, String> templateVariables) {
-		Docx docx = openTemplate(path);
-		fillTemplate(docx, templateVariables);
-		return docx;
-	}
+    public Docx fillTemplate(String path, Map<String, String> templateVariables) {
+        Docx docx = openTemplate(path);
+        fillTemplate(docx, templateVariables);
+        return docx;
+    }
 
-	public void fillTemplate(Docx docx, Map<String, String> templateVariables) {
-		try {
-			VariablesExtractor extractor = new VariablesExtractor();
-			extractor.replaceVariables(docx, templateVariables);
-		} catch (Exception e) {
-			throw new OpenDocxException(e.getMessage(), e.getCause());
-		}
-	}
+    public void fillTemplate(Docx docx, Map<String, String> templateVariables) {
+        try {
+            VariablesExtractor extractor = new VariablesExtractor();
+            extractor.replaceVariables(docx, templateVariables, variablePattern);
+        } catch (Exception e) {
+            throw new OpenDocxException(e.getMessage(), e.getCause());
+        }
+    }
 
-	public void save(Docx docx, String outputPath) {
-		try {
-			docx.getDocument().write(new FileOutputStream(outputPath));
-		} catch (Exception ex) {
-			throw new OpenDocxException(ex.getMessage(), ex.getCause());
-		}
-	}
+    public void save(Docx docx, String outputPath) {
+        try {
+            docx.getDocument().write(new FileOutputStream(outputPath));
+        } catch (Exception ex) {
+            throw new OpenDocxException(ex.getMessage(), ex.getCause());
+        }
+    }
 
-	public void save(Docx docx, OutputStream outputStream) {
-		try {
-			docx.getDocument().write(outputStream);
-		} catch (Exception ex) {
-			throw new OpenDocxException(ex.getMessage(), ex.getCause());
-		}
-	}
+    public void save(Docx docx, OutputStream outputStream) {
+        try {
+            docx.getDocument().write(outputStream);
+        } catch (Exception ex) {
+            throw new OpenDocxException(ex.getMessage(), ex.getCause());
+        }
+    }
 
-	public void setVariablePattern(VariablePattern variablePattern) {
-		this.variablePattern = variablePattern;
-	}
+    public void setVariablePattern(VariablePattern variablePattern) {
+        this.variablePattern = variablePattern;
+    }
 
 }
--- src/main/java/pl/jsolve/templ4docx/core/VariablesExtractor.java
+++ src/main/java/pl/jsolve/templ4docx/core/VariablesExtractor.java
@@ -13,50 +13,107 @@ import org.apache.poi.xwpf.usermodel.XWPFTable;
 import org.apache.poi.xwpf.usermodel.XWPFTableCell;
 import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
+import pl.jsolve.sweetener.text.Strings;
+
 public class VariablesExtractor {
 
-	public List<String> extract(String content, VariablePattern variablePattern) {
-		final List<String> tagValues = new ArrayList<String>();
-		Pattern pattern = Pattern.compile(variablePattern.getPrefix() + "(.*?)" + variablePattern.getSuffix());
-		Matcher matcher = pattern.matcher(content);
-		while (matcher.find()) {
-			tagValues.add(matcher.group());
-		}
+    public List<String> extract(String content, VariablePattern variablePattern) {
+        final List<String> tagValues = new ArrayList<String>();
+        Pattern pattern = Pattern.compile(variablePattern.getPrefix() + "(.*?)" + variablePattern.getSuffix());
+        Matcher matcher = pattern.matcher(content);
+        while (matcher.find()) {
+            tagValues.add(matcher.group());
+        }
 
-		return tagValues;
-	}
+        return tagValues;
+    }
 
-	public void replaceVariables(Docx docx, Map<String, String> tags) {
-		String[] keyArray = tags.keySet().toArray(new String[0]);
-		String[] valueArray = tags.values().toArray(new String[0]);
+    public void replaceVariables(Docx docx, Map<String, String> tags, VariablePattern variablePattern) {
+        String[] keyArray = tags.keySet().toArray(new String[0]);
+        String[] valueArray = tags.values().toArray(new String[0]);
 
-		for (XWPFParagraph p : docx.getDocument().getParagraphs()) {
-			List<XWPFRun> runs = p.getRuns();
-			if (runs != null) {
-				for (XWPFRun r : runs) {
+        for (XWPFParagraph p : docx.getDocument().getParagraphs()) {
+            execute(p.getRuns(), keyArray, valueArray, variablePattern);
+        }
 
-					String text = r.getText(0);
-					if (text != null) {
-						text = StringUtils.replaceEach(text, keyArray, valueArray);
-						r.setText(text, 0);
-					}
-				}
-			}
-		}
+        for (XWPFTable tbl : docx.getDocument().getTables()) {
+            for (XWPFTableRow row : tbl.getRows()) {
+                for (XWPFTableCell cell : row.getTableCells()) {
+                    for (XWPFParagraph p : cell.getParagraphs()) {
+                        execute(p.getRuns(), keyArray, valueArray, variablePattern);
+                    }
+                }
+            }
+        }
+    }
 
-		for (XWPFTable tbl : docx.getDocument().getTables()) {
-			for (XWPFTableRow row : tbl.getRows()) {
-				for (XWPFTableCell cell : row.getTableCells()) {
-					for (XWPFParagraph p : cell.getParagraphs()) {
-						for (XWPFRun r : p.getRuns()) {
-							String text = r.getText(0);
-							text = StringUtils.replaceEach(text, keyArray, valueArray);
-							r.setText(text, 0);
-						}
-					}
-				}
-			}
-		}
-	}
+    private void execute(List<XWPFRun> runs, String[] keyArray, String[] valueArray, VariablePattern variablePattern) {
+        if (runs == null || runs.isEmpty()) {
+            return;
+        } else if (runs.size() == 1) {
+            XWPFRun xwpfRun = runs.get(0);
+            String text = xwpfRun.getText(0);
+            if (text != null) {
+                text = StringUtils.replaceEach(text, keyArray, valueArray);
+                xwpfRun.setText(text, 0);
+            }
+        } else {
+            for (XWPFRun xwpfRun : runs) {
+                String text = xwpfRun.getText(0);
+                if (text != null) {
+                    text = StringUtils.replaceEach(text, keyArray, valueArray);
+                    xwpfRun.setText(text, 0);
+                }
+            }
+
+            // validate whether xwpfRun contains any variable pattern which are not recognized
+            String notRecognizedVariable = "";
+            String notRecognizedVariablePrefix = "";
+            int notRecognizedVariableStartIndex = -1;
+            for (int i = 0; i < runs.size(); i++) {
+                String text = runs.get(i).getText(0);
+                if (text != null) {
+                    text = text.trim();
+                    // check whether variable is started but not ended
+
+                    if (notRecognizedVariableStartIndex != -1) {
+                        if (!Strings.indexesOf(text, variablePattern.getSuffix()).isEmpty()) {
+                            notRecognizedVariable += text.substring(0, Strings.indexesOf(text, variablePattern.getSuffix()).get(0) + 1);
+                            notRecognizedVariable = StringUtils
+                                    .replaceEach(notRecognizedVariable, keyArray, valueArray);
+                            XWPFRun startRun = runs.get(notRecognizedVariableStartIndex);
+                            String textFromStartRun = startRun.getText(0);
+                            textFromStartRun = textFromStartRun.replace(notRecognizedVariablePrefix,
+                                    notRecognizedVariable);
+                            startRun.setText(textFromStartRun, 0);
+
+                            // clean runs between start and end variable pattern
+                            for (int j = notRecognizedVariableStartIndex + 1; j < i; j++) {
+                                runs.get(j).setText("", 0);
+                            }
+                            text = runs.get(i).getText(0);
+                            Integer suffixIndex = Strings.indexesOf(text, variablePattern.getSuffix()).get(0);
+                            runs.get(i).setText(text.substring(suffixIndex + 1), 0);
+                            i = notRecognizedVariableStartIndex;
+
+                            notRecognizedVariableStartIndex = -1;
+                            notRecognizedVariable = "";
+                            notRecognizedVariablePrefix = "";
+                            continue;
+                        }
+                        notRecognizedVariable += text;
+                    }
+
+                    if (!Strings.indexesOf(text, variablePattern.getPrefix()).isEmpty()
+                            && Strings.indexesOf(text, variablePattern.getSuffix()).isEmpty()) {
+                        notRecognizedVariableStartIndex = i;
+                        notRecognizedVariable = text;
+                        Integer prefixIndex = Strings.indexesOf(text, variablePattern.getPrefix()).get(0);
+                        notRecognizedVariablePrefix = text.substring(prefixIndex);
+                    }
+                }
+            }
+        }
+    }
 
 }
