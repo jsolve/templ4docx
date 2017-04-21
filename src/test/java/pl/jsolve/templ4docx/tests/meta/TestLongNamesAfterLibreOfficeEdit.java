180c09740c48980959d459f8375a9e329aa8c007
indvd00m <gotoindvdum@gmail.com>
2017-04-21 17:16:25 +0300

    Add tests for variables with long names

---
+++ src/test/java/pl/jsolve/templ4docx/tests/meta/TestLongNamesAfterLibreOfficeEdit.java
@@ -0,0 +1,62 @@
+package pl.jsolve.templ4docx.tests.meta;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+import pl.jsolve.templ4docx.core.Docx;
+import pl.jsolve.templ4docx.core.VariablePattern;
+import pl.jsolve.templ4docx.variable.TextVariable;
+import pl.jsolve.templ4docx.variable.Variables;
+
+/**
+ * @author indvd00m
+ *
+ */
+public class TestLongNamesAfterLibreOfficeEdit {
+
+    @Test
+    public void test() throws IOException {
+        String templateFileName = "template03-processed-edited-in-libre-office";
+        InputStream is = getClass().getClassLoader().getResourceAsStream(templateFileName + ".docx");
+
+        Docx docx = new Docx(is);
+
+        String text = docx.readTextContent();
+        assertEquals(
+                "This is test simple edited template with three variables with long names: value01, value02, value03. File edited in Libre Office.",
+                text.trim());
+
+        docx.setProcessMetaInformation(true);
+        docx.setVariablePattern(new VariablePattern("${", "}"));
+
+        Variables var = new Variables();
+        var.addTextVariable(new TextVariable("${variableWithVeryVeryLongName01}", "valueAfterEditInLibreOffice01"));
+        var.addTextVariable(
+                new TextVariable("${variableWithVeryVeryVeryVeryVeryVeryLongName02}", "valueAfterEditInLibreOffice02"));
+        var.addTextVariable(
+                new TextVariable("${variableWithVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongName03}",
+                        "valueAfterEditInLibreOffice03"));
+
+        docx.fillTemplate(var);
+
+        String tmpPath = System.getProperty("java.io.tmpdir");
+        String processedPath = String.format("%s%s%s", tmpPath, File.separator,
+                templateFileName + "-processed2" + ".docx");
+
+        docx.save(processedPath);
+
+        text = docx.readTextContent();
+        assertEquals(
+                "This is test simple edited template with three variables with long names: valueAfterEditInLibreOffice01, valueAfterEditInLibreOffice02, valueAfterEditInLibreOffice03. File edited in Libre Office.",
+                text.trim());
+
+        is.close();
+
+    }
+
+}
